721eaf64dcad237b166c4ad4922ae722e5822e83
Gustavo Weber Denardin <gustavo.denardin@gmail.com>
2021-04-22 20:31:31 -0300

    Client TCP agora suporta definir IP no terminal

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,15 +2,26 @@ use std::net::{TcpStream};
 use std::io::{Read, Write};
 use std::str::from_utf8;
 use std::{thread, time};
+use std::env;
 
 fn main() {
-    match TcpStream::connect("localhost:3333") {
+    let args: Vec<String> = env::args().collect();
+    let mut ip_addr = "localhost:3333".to_string();
+    if args.len() == 2 {
+        let ip_addr_str = &args[1];
+        ip_addr.clear();
+        ip_addr.push_str(ip_addr_str);
+        ip_addr.push_str(":3333");
+    }
+    let ip_addr_str = ip_addr.clone();
+
+    match TcpStream::connect(ip_addr) {
         Ok(mut stream) => {
-            println!("Successfully connected to server in port 3333");
+            println!("Successfully connected to server in IP {}", ip_addr_str);
 
             let msg = b"Hello!";
             let ten_millis = time::Duration::from_millis(2000);
-            let now = time::Instant::now();
+            //let now = time::Instant::now();
 
             loop {
                 stream.write(msg).unwrap();
